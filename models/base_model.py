f2ec24bebeac10872fea4836ead0e93340b75757
Joseph50079 <josephogbah2019@gmail.com>
2024-03-30 20:56:50 +0100

    Airbnb part 1

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import datetime
+import uuid
+
+"""Module Base_model for all Airbnb models
+
+"""
+
+class BaseModel:
+    """Basemodel class  that defines all common attributes
+        methods for other classes
+
+    Args:
+
+    """
+    
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        
+    def to_dict(self):
+        dic = self.__dict__.copy()
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        
+        return dic
+        
