bdba8ee99d726a3ddc36d8f662ff7af44fd788b7
Joseph50079 <josephogbah2019@gmail.com>
2024-05-01 08:36:33 +0100

    doc

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""__init__.py: to create a unique 
+FileStorage instance for your application
+
+"""
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
