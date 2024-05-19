ebd3d2e0defce82643a2886b071cc9bfb5bdb038
Joseph50079 <josephogbah2019@gmail.com>
2024-05-19 23:34:02 +0100

    Update console.py

--- console.py
+++ console.py
@@ -136,8 +136,32 @@ class HBNBCommand(cmd.Cmd):
                 list_all.append(val)
             print(list_all)
 
-    def do_update(self, arg):
-        pass
+    def do_update(self, argv):
+        """Updates an instance based on the class name and id by adding or
+        updating attribute and save it to the JSON file."""
+        arg_list = parser_arg(argv)
+        if arg_list:
+            if len(arg_list) == 1:
+                print("** instance id missing **")
+            else:
+                instance_id = "{}.{}".format(arg_list[0], arg_list[1])
+                if instance_id in self.storage.all():
+                    if len(arg_list) == 2:
+                        print("** attribute name missing **")
+                    elif len(arg_list) == 3:
+                        print("** value missing **")
+                    else:
+                        obj = self.storage.all()[instance_id]
+                        if arg_list[2] in type(obj).__dict__:
+                            v_type = type(obj.__class__.__dict__[arg_list[2]])
+                            setattr(obj, arg_list[2], v_type(arg_list[3]))
+                        else:
+                            setattr(obj, arg_list[2], arg_list[3])
+                else:
+                    print("** no instance found **")
+
+            self.storage.save()
+
 
         
 if __name__ == "__main__":   
